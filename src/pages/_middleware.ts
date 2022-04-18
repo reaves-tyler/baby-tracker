a49e22af6c670ec73be64a31c59144160a34f807
Tyler Reaves <tylersi93@gmail.com>
2022-04-18 14:39:27 -0400

    Update _middleware.ts

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,6 +3,6 @@ import { withAuth } from 'next-auth/middleware';
 export default withAuth({
     callbacks: {
         authorized: ({ token }) =>
-            token?.email === 'tylersi93@gmail.com' || token?.email === 'marinobethany002@gmail.com',
+            token?.email === 'tylersi93@gmail.com' || token?.email === 'marinobethany002@gmail.com' || token?.email === 'shelmarino@gmail.com',
     },
 });
